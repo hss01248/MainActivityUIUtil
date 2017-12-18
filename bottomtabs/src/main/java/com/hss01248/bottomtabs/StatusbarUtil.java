501bd2664c087c31456595a5538ad01b6556489b
hss01248 <hss01248@gmail.com>
2017-12-18 16:06:55 +0800

    nothing

--- bottomtabs/src/main/java/com/hss01248/bottomtabs/StatusbarUtil.java
+++ bottomtabs/src/main/java/com/hss01248/bottomtabs/StatusbarUtil.java
@@ -162,6 +162,7 @@ public class StatusbarUtil {
 
     public static void initInTabs(Activity activity){
         setBgTransparent(activity);
+        //StatusBarCompat.setTranslucent(activity.getWindow(),true);
     }
 
 
@@ -311,9 +312,10 @@ public class StatusbarUtil {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = activity.getWindow();
             //window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             //window.setStatusBarColor(Color.TRANSPARENT);
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
